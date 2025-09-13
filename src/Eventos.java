34ed21d5b02694a166523612376f633ea5fd0da1
Raffaella Tellini Aranha <raffaaranha@hotmail.com>
2025-09-13 16:32:44 -0300

    iniciando classe eventos

---
+++ src/Eventos.java
@@ -0,0 +1,8 @@
+public class Eventos{
+    private int codigoUnico;
+    private String nomeEvento;
+    private Date dataRealizacao;
+    private double valorIngresso;
+    private String nomeRespontavel;
+    private int quantidadeTotalIngressos;
+}
