ce47e1221d902916f769ec8e1a9df10769c62063
Raffaella Tellini Aranha <raffaaranha@hotmail.com>
2025-09-13 19:26:25 -0300

    ajustando arquivos

--- src/Eventos.java
+++
@@ -1,8 +0,0 @@
-public class Eventos{
-    private int codigoUnico;
-    private String nomeEvento;
-    private Date dataRealizacao;
-    private double valorIngresso;
-    private String nomeRespontavel;
-    private int quantidadeTotalIngressos;
-}
