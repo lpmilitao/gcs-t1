7d8d48c8adc837104b9590a5c0bb32e29f92fb42
ellenzv <ellenvitoriasm@gmail.com>
2025-09-28 16:34:18 -0300

    estrutura principal da classe ingresso criada

--- Projeto Codigo GCS1/src/Ingresso.java
+++ Projeto Codigo GCS1/src/Ingresso.java
@@ -1,2 +1,48 @@
 public class Ingresso {
+
+    private String codigo;
+    private boolean especial;
+    private Participante participante;
+    private boolean utilizado;
+
+    public Ingresso(String codigo, boolean especial) {
+        this.codigo = codigo;
+        this.especial = especial;
+        this.utilizado = false;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public boolean isEspecial() {
+        return especial;
+    }
+
+    public Participante getParticipante() {
+        return participante;
+    }
+
+    public void setParticipante(Participante participante) {
+        this.participante = participante;
+    }
+
+    public boolean isUtilizado() {
+        return utilizado;
+    }
+
+    public void registrarEntrada() {
+        this.utilizado = true;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Ingresso{" +
+                "codigo='" + codigo + '\'' +
+                ", especial=" + especial +
+                ", participante=" + (participante != null ? participante.getNomeCompleto() : "Não associado") +
+                ", utilizado=" + utilizado +
+                '}';
+    }
 }
