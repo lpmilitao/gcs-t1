dbdf5d1abc98be16f2f232b8b24710985ae1821a
unknown <analech@dohms.org.br>
2025-10-01 18:12:34 -0300

    estrutura classe participante

--- src/main/java/Participante.java
+++ src/main/java/Participante.java
@@ -1,14 +1,40 @@
+import java.util.List;
+
 public class Participante {
     public String nome;
+    public String cpf;
 
 
-    public Participante(String nome){
+    public Participante(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
 
-}
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 
+    public String getCpf() {
+        return cpf;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Participante{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                '}';
+
+
+    }
+}
